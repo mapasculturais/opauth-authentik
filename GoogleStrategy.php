37418d555e7444fd3940aa923b0aa712296affa8
Leandro Monteiro dal Bò <leandro.db@gmail.com>
2014-09-22 14:11:42 -0300

    work in progress: working with devel-authProviderOAuth branch of hacklabr/mapasculturais (logout not working)

--- GoogleStrategy.php
+++ GoogleStrategy.php
@@ -2,9 +2,9 @@
 /**
  * Google strategy for Opauth
  * based on https://developers.google.com/accounts/docs/OAuth2
- * 
+ *
  * More information on Opauth: http://opauth.org
- * 
+ *
  * @copyright    Copyright © 2012 U-Zyn Chua (http://uzyn.com)
  * @link         http://opauth.org
  * @package      Opauth.GoogleStrategy
@@ -14,21 +14,21 @@
 /**
  * Google strategy for Opauth
  * based on https://developers.google.com/accounts/docs/OAuth2
- * 
+ *
  * @package			Opauth.Google
  */
 class GoogleStrategy extends OpauthStrategy{
-	
+
 	/**
 	 * Compulsory config keys, listed as unassociative arrays
 	 */
 	public $expects = array('client_id', 'client_secret');
-	
+
 	/**
 	 * Optional config keys, without predefining any default values.
 	 */
 	public $optionals = array('redirect_uri', 'scope', 'state', 'access_type', 'approval_prompt');
-	
+
 	/**
 	 * Optional config keys with respective default values, listed as associative arrays
 	 * eg. array('scope' => 'email');
@@ -37,12 +37,12 @@ class GoogleStrategy extends OpauthStrategy{
 		'redirect_uri' => '{complete_url_to_strategy}oauth2callback',
 		'scope' => 'public_profile email full_name'
 	);
-	
+
 	/**
 	 * Auth request
 	 */
 	public function request(){
-		$url = 'https://meu.rs.gov.br/oauth/v2/auth';
+		$url = $this->strategy['host'].$this->strategy['auth_path'];
 		$params = array(
 			'client_id' => $this->strategy['client_id'],
 			'redirect_uri' => $this->strategy['redirect_uri'],
@@ -53,17 +53,17 @@ class GoogleStrategy extends OpauthStrategy{
 		foreach ($this->optionals as $key){
 			if (!empty($this->strategy[$key])) $params[$key] = $this->strategy[$key];
 		}
-		
+
 		$this->clientGet($url, $params);
 	}
-	
+
 	/**
 	 * Internal callback, after OAuth
 	 */
 	public function oauth2callback(){
 		if (array_key_exists('code', $_GET) && !empty($_GET['code'])){
 			$code = $_GET['code'];
-			$url = 'https://meu.rs.gov.br/oauth/v2/token';
+			$url = $this->strategy['host'].$this->strategy['token_path'];
 			$params = array(
 				'code' => $code,
 				'client_id' => $this->strategy['client_id'],
@@ -72,12 +72,12 @@ class GoogleStrategy extends OpauthStrategy{
 				'grant_type' => 'authorization_code'
 			);
 			$response = $this->serverPost($url, $params, null, $headers);
-			
+
 			$results = json_decode($response);
-			
+
 			if (!empty($results) && !empty($results->access_token)){
 				$userinfo = $this->userinfo($results->access_token);
-				
+
 				$this->auth = array(
 					'uid' => $userinfo['id'],
 					'info' => array(),
@@ -92,13 +92,13 @@ class GoogleStrategy extends OpauthStrategy{
 				{
 					$this->auth['credentials']['refresh_token'] = $results->refresh_token;
 				}
-				
+
 				$this->mapProfile($userinfo, 'name', 'first_name');
 				$this->mapProfile($userinfo, 'email', 'email');
 				$this->mapProfile($userinfo, 'given_name', 'first_name');
 				$this->mapProfile($userinfo, 'family_name', 'surname');
 				$this->mapProfile($userinfo, 'picture', 'profile_picture_url');
-				
+
 				$this->callback();
 			}
 			else{
@@ -119,19 +119,19 @@ class GoogleStrategy extends OpauthStrategy{
 				'code' => 'oauth2callback_error',
 				'raw' => $_GET
 			);
-			
+
 			$this->errorCallback($error);
 		}
 	}
-	
+
 	/**
 	 * Queries Google API for user info
 	 *
-	 * @param string $access_token 
+	 * @param string $access_token
 	 * @return array Parsed JSON results
 	 */
 	private function userinfo($access_token){
-		$userinfo = $this->serverGet('https://meu.rs.gov.br/api/v1/person', array('access_token' => $access_token), null, $headers);
+		$userinfo = $this->serverGet($this->strategy['host'].$this->strategy['userinfo_path'], array('access_token' => $access_token), null, $headers);
 		if (!empty($userinfo)){
 			return $this->recursiveGetObjectVars(json_decode($userinfo));
 		}
